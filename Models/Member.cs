ca095e4aacebb96d5880bea5ac85ee8c916e61b1
Danny Parker <danny.ukdm@gmail.com>
2018-03-26 16:59:53 +0100

    add initial member entity

---
+++ Models/Member.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DevTest.Models
+{
+	public class Member
+	{
+		public int Id { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public DateTime DateCreated { get; set; }
+	}
+}
