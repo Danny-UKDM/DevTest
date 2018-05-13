7339d454512f793873d092c71f8ec38f586e95a3
Danny Parker <danny.ukdm@gmail.com>
2018-05-13 16:46:27 +0100

    create API for posting Member data

---
+++ Controllers/Api/MembersController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using DevTest.Models;
+using DevTest.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace DevTest.Controllers.Api
+{
+	[Route("api/members")]
+	public class MembersController : Controller
+	{
+		private readonly IMemberRepository _repository;
+		private readonly ILogger<MembersController> _logger;
+
+		public MembersController(IMemberRepository repository, ILogger<MembersController> logger)
+		{
+			_repository = repository;
+			_logger = logger;
+		}
+
+		[HttpPost("")]
+		public async Task<IActionResult> Post([FromBody]MemberViewModel member)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					var newMember = Mapper.Map<Member>(member);
+
+					_repository.AddMember(newMember);
+
+					if (await _repository.SaveChangesAsync())
+					{
+						return Created($"api/members", Mapper.Map<MemberViewModel>(newMember));
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError($"Failed to save new Member: {0}", ex);
+			}
+
+			return BadRequest("Failed to save new Member");
+		}
+	}
+}
