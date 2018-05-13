8310f636d197ce2c2aee6103f45743e861b1bf14
Danny Parker <danny.ukdm@gmail.com>
2018-05-13 16:46:50 +0100

    add Member viewmodel

---
+++ ViewModels/MemberViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DevTest.ViewModels
+{
+	public class MemberViewModel
+	{
+		[Required]
+		[EmailAddress]
+		public string Email { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+		public DateTime DateCreated { get; set; } = DateTime.Now;
+	}
+}
