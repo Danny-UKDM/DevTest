e959f44f79bbad578e5678fdaee1593ea8198f73
Danny Parker <danny.ukdm@gmail.com>
2018-06-06 20:53:23 +0100

    removes Idenity User derivation from Member class

--- Models/Member.cs
+++ Models/Member.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace DevTest.Models
+﻿namespace DevTest.Models
 {
-	public class Member : IdentityUser
+	public class Member
 	{
+		public string Email { get; set; }
+		public string Password { get; set; }
 	}
 }
